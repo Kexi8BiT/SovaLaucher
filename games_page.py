82a893bf7f8e05c7fef7678f77753e783242e3f5
Mirvis <108653332+Kexi8BiT@users.noreply.github.com>
2024-03-15 00:34:57 +0500

    Удалена папка servers и обновлены модули

--- games_page.py
+++ games_page.py
@@ -130,6 +130,7 @@ def get_games_page(page, content):
                     Game_object(id="sovacraft", web_content=False, page=page, content=content),
                     Game_object(id="sovadash", web_content=False, page=page, content=content),
                     Game_object(id="sovadastry", web_content=False, page=page, content=content),
+                    Game_object(id="sovapidor", web_content=False, page=page, content=content),
 
                 ],
                 alignment=ft.MainAxisAlignment.START,
